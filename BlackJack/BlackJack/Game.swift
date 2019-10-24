42ac45fbfd27fded41f7121fd92d97e4bf64368e
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-10-24 12:12:33 -0400

    Start of Project. Created files, base instances etc

---
+++ BlackJack/BlackJack/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  BlackJack
+//
+//  Created by Juan Ceballos on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+class Game  {
+    let deck: [Card] = []
+    let player = Player(score: 0, cards: [], playerName: "")
+    let hitPlayer = Bool()
+}
---
+++ BlackJack/BlackJack/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  BlackJack
+//
+//  Created by Juan Ceballos on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    let score: Int
+    let cards: [Card]
+    let playerName: String
+}
