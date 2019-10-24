d6fe06d47099f17c43c2063885367beedfa660e4
Casandra <casandragrullon@pursuit.org>
2019-10-24 10:40:41 -0400

    added a file for Suits

---
+++ BlackJack/BlackJack/Suit.swift
@@ -0,0 +1,15 @@
+//
+//  Suit.swift
+//  BlackJack
+//
+//  Created by casandra grullon on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+enum Suit: String {
+    case clubs = "♣️"
+    case hearts = "♥️"
+    
+}
