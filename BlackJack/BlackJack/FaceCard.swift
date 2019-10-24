afa1ee28eee1bb77d3a78d6cff5b4e2290a01b40
Joshua Wynter <joshpursuit@Joshuas-MacBook-Pro.local>
2019-10-24 11:54:53 -0400

    updated to 10.14 version

---
+++ BlackJack/BlackJack/FaceCard.swift
@@ -0,0 +1,13 @@
+//
+//  FaceCard.swift
+//  BlackJack
+//
+//  Created by Joshua Wynter on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+enum FaceCard {
+    case king =
+}
