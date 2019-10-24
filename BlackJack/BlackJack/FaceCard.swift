155438291240d1ecab4e43790a505d39369dd279
Edward O'Neill <edwardoneill@Edwards-MacBook-Pro.local>
2019-10-24 10:39:43 -0400

    initial commit for cloning

---
+++ BlackJack/BlackJack/FaceCard.swift
@@ -0,0 +1,9 @@
+//
+//  FaceCard.swift
+//  BlackJack
+//
+//  Created by Edward O'Neill on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
---
+++ BlackJack/BlackJack/Suit.swift
@@ -0,0 +1,16 @@
+//
+//  Suit.swift
+//  BlackJack
+//
+//  Created by Edward O'Neill on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+enum Suit: String {
+    case dimond = "♦️"
+    case hart = "♥️"
+    case spade = "♠️"
+    case club = "♣️"
+}
