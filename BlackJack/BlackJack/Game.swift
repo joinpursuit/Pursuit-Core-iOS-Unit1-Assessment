af2391ea82c2707e6488cc950f277dc31b9ad1dd
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-24 11:56:08 -0400

    update

--- BlackJack/BlackJack/Game.swift
+++ BlackJack/BlackJack/Game.swift
@@ -17,15 +17,31 @@ class Game {
         self.deck = deck
         self.player = player
         self.hitPLayer = hitPlayer
-        
+    }
         var hasMoreCards: Bool {
             
             return !deck.isEmpty
         }
         
-        
-        
-        
-        
+        var randomComputerScore: Int {
+            
+            return player.score
+        }
+    
+    func newGame() {
+        self.deck = [Card]()
     }
-}
+    // gotta work on this function more
+    
+    func stopHits(userChoice: String) {
+        var userChoice = userChoice
+        userChoice = readLine()?.lowercased() ?? ""
+        
+        if userChoice == "pass"{
+            print(deck.randomElement() ?? "")
+        }
+    }
+    
+    
+    
+    }
