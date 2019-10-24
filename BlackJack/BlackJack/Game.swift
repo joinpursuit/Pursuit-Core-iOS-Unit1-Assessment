a83bf483813de16a7e4f5a6c36265eb3c1a9c896
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-24 12:18:47 -0400

    updated to version 10.14

---
+++ BlackJack/BlackJack/Game.swift
@@ -0,0 +1,55 @@
+//
+//  Game.swift
+//  BlackJack
+//
+//  Created by Maitree Bain on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+var deck = [Card]()
+var player: Player
+var hitPLayer: Bool
+
+init(deck: [Card], player: Player, hitPlayer: Bool) {
+    self.deck = deck
+    self.player = player
+    self.hitPLayer = hitPlayer
+}
+    var hasMoreCards: Bool {
+        
+        return !deck.isEmpty
+    }
+    
+    var randomComputerScore: Int {
+        
+        return player.score
+    }
+
+func newGame() {
+    self.deck = [Card]()
+}
+// gotta work on this function more
+
+func stopHits(userChoice: String) {
+    var userChoice = userChoice
+    userChoice = readLine()?.lowercased() ?? ""
+    
+    if userChoice == "pass"{
+        print(deck.randomElement() ?? "")
+    }
+}
+
+func hitMe(userHit: String) {
+    var userHit = userHit
+    userHit = readLine()?.lowercased() ?? ""
+    
+    if userHit == "hit" {
+        print()
+    }
+}
+
+
+}
