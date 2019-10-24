113cc1047bab8a68ad68fc52f65741e600ca96ed
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-10-24 16:14:59 -0400

    Updated Project

--- BlackJack/BlackJack/Game.swift
+++ BlackJack/BlackJack/Game.swift
@@ -9,7 +9,50 @@
 import Foundation
 
 class Game  {
-    let deck: [Card] = []
-    let player = Player(score: 0, cards: [], playerName: "")
-    let hitPlayer = Bool()
+    var deck: [Card] = []
+    var player = Player(score: 0, cards: [], playerName: "")
+    var hitPlayer = Bool()
+    
+    var hasMoreCards: Bool    {
+        return !deck.isEmpty
+    }
+    var randomComputerScore: Int    {
+        let computerScore = Int.random(in: 16...21)
+        return computerScore
+    }
+    
+    func newGame()  {
+        deck = Card.newDeck(aceValue: 11)
+        player = Player(score: 0, cards: [], playerName: "")
+        hitPlayer = Bool()
+    }
+    
+    func stopHits() {
+        
+        /*stopHits() called if the user wishes to pass their turn. In that case the computer draws a random number and a winner is chosen between the computer's score and the player's score.*/
+        hitPlayer = false
+        computerVsPlayer()
+        
+    }
+    
+    func hitMe() -> Card?   {
+        //called as the user requests more cards from the deck
+        var playersCard = deck.shuffled()
+        return playersCard.popLast()
+    }
+    
+    func computerVsPlayer() {
+        //draws a random number for the computer and determines the winner of the game.
+        //let computerScore = randomComputerScore
+        //let playerScore = player.score
+        
+    }
+    
+    func gameStatus()   {
+        
+        /*takes in the player's card and determines the current score. Here the player score options can be, BlackJack, Bust or Continue playing as their status is still valid for game play.*/
+        
+        
+    }
 }
+
--- BlackJack/BlackJack/main.swift
+++ BlackJack/BlackJack/main.swift
@@ -11,3 +11,28 @@ import Foundation
 // TODO: remove these lines after you have added the Suit and FaceCard enums as per the assessment README
 print("There are \(Card.newDeck(aceValue: 1).count) in a deck of cards")
 // There are 52 in a deck of cards
+
+let game = Game()
+game.newGame()
+var gameOver = true
+
+var prompt = "Do you wish to hit or pass?(hit, pass)"
+
+repeat  {
+    print(prompt)
+    let playreResponse = readLine()
+    
+    switch playreResponse   {
+    case "hit":
+        let hitCard = game.hitMe()
+        let stringHitCard = hitCard?.stringify()
+        print(stringHitCard ?? "Sorry")
+        
+    case "pass":
+        game.stopHits()
+        
+    default: print("Error")
+    }
+    
+} while gameOver == false
+
