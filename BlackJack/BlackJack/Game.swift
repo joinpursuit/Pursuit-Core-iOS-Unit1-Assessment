65dad3e7d10ce8fb5a6ab7e3752d7a22075f6408
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-25 16:51:52 -0400

    finished most functions and fixed repeat loop

--- BlackJack/BlackJack/Game.swift
+++ BlackJack/BlackJack/Game.swift
@@ -29,13 +29,14 @@ class Game {
     }
     
     func newGame() {
-        self.deck = [Card]()
+      deck = Card.newDeck(aceValue: 1)
+     player = Player(score: 0, cards: [], playerName: "")
     }
     // gotta work on this function more
     
      func hitMe(userChoice: String) -> Card? {
         //called as the user requests more cards from the deck
-          if userChoice == "hit" {
+          //if userChoice == "hit" {
                deck = Card.newDeck(aceValue: 1)
                deck = deck.shuffled()
                let card = deck.randomElement()
@@ -43,25 +44,35 @@ class Game {
                player.score += card!.value
                print(card?.stringify() ?? "")
                print("Player Score: \(player.score)")
+               if player.score == 21 {
+                    print("Player Wins!")
+                    deck.removeAll()
+               } else if player.score > 21 {
+                    print("BUST! Player loses!")
+                    deck.removeAll()
                }
-               //add tie later
+               
+         //      }
           return deck.popLast()
           }
 
     func stopHits(userChoice: String) {
         //called if the user wishes to pass their turn. In that case the computer draws a random number and a winner is chosen between the computer's score and the player's score.
-        if userChoice == "pass"{
+     //   if userChoice == "pass"{
           deck = Card.newDeck(aceValue: 1)
           deck = deck.shuffled()
           if player.score > randomComputerScore{
                print("\(player.score) - The player wins this round!")
           } else if randomComputerScore > player.score {
                print("Player: \(player.score)\n Computer: \(randomComputerScore) - The computer wins this round!")
+          } else if randomComputerScore == player.score {
+               print("Player: \(player.score)\n Computer: \(randomComputerScore) - It's a tie!")
           }
-        }
+          
+     //   }
     }
     
-    func computerVsPlayer() {
+     func computerVsPlayer(randomComputerNumber: Int) {
         //draws a random number for the computer and determines the winner of the game.
      var randomComputerNumber = Card.newDeck(aceValue: 1)
      randomComputerNumber = randomComputerNumber.shuffled()
