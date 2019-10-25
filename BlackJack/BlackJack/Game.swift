82f545245741aace241176142f4215d5688924a1
Yuliia Engman <yuliiaengmanpursuit@Yuliias-MacBook-Pro.local>
2019-10-25 15:13:47 -0400

    hit works in the game

--- BlackJack/BlackJack/Game.swift
+++ BlackJack/BlackJack/Game.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Game {
     var deck = [Card]()
-    var player = Player(score: 0, cards: [], playerName: "Yulia") // something is incorrectr here
+    var player = Player(score: 0, cards: [], playerName: "Yuliia") // something is incorrectr here
     var hitPlayer = Bool()
     
     var hasMoreCards: Bool {
@@ -26,14 +26,17 @@ class Game {
     
     func newGame() {
         player.score = 0
-        deck.removeAll() // double check that this is waht I want to remove
+        deck = Card.newDeck(aceValue: 11)
+        print("Do you want to hit or pass? (hit, pass)")
     }
     
     func computerVsPlayer() { // for now I think it completelly substiture stopFits method
         if randomComputerScore > player.score {
-            print("You lost!")
+            print("Computer got \(randomComputerScore). You lost!")
+            print("Do you want to paly again? (yes/no)")
         } else {
-            print("You win!")
+            print("Ypu got \(player.score). You win!")
+            print("Do you want to paly again? (yes/no)")
         }
     }
     
@@ -41,16 +44,18 @@ class Game {
         var shuffledDeck = deck.shuffled() // shuffle the deck
         let randomCardForPlayer = shuffledDeck.removeLast() //removes element from deck array and returns it
         player.score += randomCardForPlayer.value// adds score
-        print("\(randomCardForPlayer) score: \(player.score)")// prints card and score // decide how to print following random cards
+        print("\(randomCardForPlayer.stringify()) score: \(player.score)")// prints card and score // decide how to print following random cards
+        
     }
     
     func gameStatus() {
         if player.score == 21 {
-            print("BlackJack")
+            print("BlackJack!!!")
         } else if player.score > 21 {
-            print("Bust")
+            print("Bust!")
         } else {
-            print("Continue")
+            print("Continue playing")
+            print("Do you want to hit or pass? (hit, pass)")
         }
     }
 }
--- BlackJack/BlackJack/main.swift
+++ BlackJack/BlackJack/main.swift
@@ -8,10 +8,33 @@
 
 import Foundation
 
-// TODO: remove these lines after you have added the Suit and FaceCard enums as per the assessment README
-print("There are \(Card.newDeck(aceValue: 1).count) in a deck of cards")
-// There are 52 in a deck of cards
+//print("""
+//db   d8b   db d88888b db       .o88b.  .d88b.  .88b  d88. d88888b d888888b  .d88b.  d8888b. db       .d8b.   .o88b. db   dD    d88b  .d8b.   .o88b. db   dD
+//88   I8I   88 88'     88      d8P  Y8 .8P  Y8. 88'YbdP`88 88'     `~~88~~' .8P  Y8. 88  `8D 88      d8' `8b d8P  Y8 88 ,8P'    `8P' d8' `8b d8P  Y8 88 ,8P'
+//88   I8I   88 88ooooo 88      8P      88    88 88  88  88 88ooooo    88    88    88 88oooY' 88      88ooo88 8P      88,8P       88  88ooo88 8P      88,8P
+//Y8   I8I   88 88~~~~~ 88      8b      88    88 88  88  88 88~~~~~    88    88    88 88~~~b. 88      88~~~88 8b      88`8b       88  88~~~88 8b      88`8b
+//`8b d8'8b d8' 88.     88booo. Y8b  d8 `8b  d8' 88  88  88 88.        88    `8b  d8' 88   8D 88booo. 88   88 Y8b  d8 88 `88. db. 88  88   88 Y8b  d8 88 `88.
+// `8b8' `8d8'  Y88888P Y88888P  `Y88P'  `Y88P'  YP  YP  YP Y88888P    YP     `Y88P'  Y8888P' Y88888P YP   YP  `Y88P' YP   YD Y8888P  YP   YP  `Y88P' YP   YD
+//""")
 
 var game = Game()
-game.player.playerName = "antonio"
-print(game.player.playerName )
+game.newGame() // prints Do you want to hit or pass? (hit, pass)
+
+var gameContinues = true
+var userResponse = readLine()?.lowercased() ?? ""
+
+// here I should write main logic of the game
+repeat {
+if userResponse == "hit" {
+    game.hitMe()
+    game.gameStatus() // if contunue prints "Do you want to hit or pass? (hit, pass)"
+    userResponse = readLine()?.lowercased() ?? ""
+} else if userResponse == "pass" {
+    game.gameStatus()
+    gameContinues = false
+} else {
+    print("Not valid answer. Try again.")
+}
+    //game.computerVsPlayer() //Game result and question: "Do you want to paly again? (yes/no)")
+} while gameContinues
+
