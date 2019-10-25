f0a3b2a7aa9434f3e1b55f73cfeb79cc129ff86e
Matthew Ramos <matthewramos@Matthews-MBP.lan1>
2019-10-25 09:59:24 -0400

    Completed blackjack assessment

---
+++ BlackJack/BlackJack/Suit.swift
@@ -0,0 +1,13 @@
+//
+//  Suit.swift
+//  BlackJack
+//
+//  Created by Matthew Ramos on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+enum Suit: String, CaseIterable {
+    case diamond = "♦️", heart = "♥️", spade = "♠️", club = "♣️"
+}
